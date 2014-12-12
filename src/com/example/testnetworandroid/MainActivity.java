e1cb0f1ef13fd84fe5e109148126bbf10f15a7c5
2K30 <konstantin_koshevoy@gmx.de>
2014-12-12 11:34:38 +0100

    Test commit

--- src/com/example/testnetworandroid/MainActivity.java
+++ src/com/example/testnetworandroid/MainActivity.java
@@ -6,12 +6,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
+
 public class MainActivity extends Activity {
 
+	private String m_test = "";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
     }
 
 
